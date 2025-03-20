a8193b5a93724daaa655cd820fe5b92f222fee5e
JZuegg <j.zuegg@uq.edu.au>
2025-03-20 22:01:08 +1000

    libfunc

--- PeptideMiner-master v1.0/libfunc/blast.py
+++ PeptideMiner-master v1.0/libfunc/blast.py
@@ -7,14 +7,15 @@ Support module for Step 7.
 """
 
 def make(knownseqfile):
-    """Create a ncbi db of knownseqfile"""
-    blastdb = os.popen('{0}/makeblastdb -in {knownseqfile} -dbtype prot -hash_index -out {knownseqfile}'.format(
-        config.C['ncbi_path'],))
+    #Create a ncbi db of knownseqfile
+    blastdb = os.popen(f'{0}/makeblastdb -in {knownseqfile} -dbtype prot -hash_index -out {knownseqfile}')
+    # blastdb = os.popen(f'{0}/makeblastdb -in {knownseqfile} -dbtype prot -hash_index -out {knownseqfile}'.format(
+    #     config.C['ncbi_path'],))
 
 
 def check(knownseqfile):
-    """Check if an NCBI db of the knownseqfile already exists"""
-    query = '{}'.format(knownseqfile)
+    #Check if an NCBI db of the knownseqfile already exists
+    query = knownseqfile
 
     if os.path.isfile(f'{knownseqfile}.phd') :
         print(f" [BLAST] NCBI database for {knownseqfile} exists already !\n")
@@ -26,8 +27,9 @@ def check(knownseqfile):
 
 def blastp(blastdb, queryfile, outfile):
     print(' [BLAST] Running BLASTp...')
-    blastp = os.popen('{0}/blastp -db {1} -query {2} -outfmt 10 -out {3}'.format(
-        config.C['ncbi_path'],blastdb,queryfile,outfile))
+    blastp = os.popen(f'blastp -db {blastdb} -query {queryfile} -outfmt 10 -out {outfile}')
+    # blastp = os.popen('{0}/blastp -db {1} -query {2} -outfmt 10 -out {3}'.format(
+    #     config.C['ncbi_path'],blastdb,queryfile,outfile))
     print('  [BLAST] BLASTp complete.')
 
     return blastp
@@ -48,24 +50,24 @@ def file(file):
 
 
 def parse(blastoutput):
-    """Parsing the blast outfmt 10 file"""
+    #Parsing the blast outfmt 10 file"""
     B = []
     blastout = file(blastoutput)
     for b in blastout:
         #[hit,known sequence,PID,length,evalue]
         B.append([int(b[0]),b[1],b[2],b[3],b[10]])
 
-    """Create list of unique query id's"""
+    #Create list of unique query id's"""
     query = set([i[0] for i in B])
     U = []
     for q in query:
-        """Loop through blastp output and group query ID's"""
+        #Loop through blastp output and group query ID's"""
         b = [i for i in B if i[0] == q]
         
-        """For each query ID, sort group according to evalue"""
+        #For each query ID, sort group according to evalue"""
         b.sort(key=lambda i:float(i[4]))
 
-        """For each group, keep only the hit with the lowest evalue"""
+        #For each group, keep only the hit with the lowest evalue"""
         U.append(b[0])
     U.sort()
     

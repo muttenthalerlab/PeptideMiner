2f7e14cb42053a378f40c61879dc810f789738f7
JZuegg <j.zuegg@uq.edu.au>
2025-03-20 22:22:45 +1000

    libfunc

--- PeptideMiner-master v1.0/libfunc/mydb.py
+++ PeptideMiner-master v1.0/libfunc/mydb.py
@@ -55,7 +55,7 @@ class mysql:
 					if re.search('^[a-z]{1,4}\.[A-za-z_]+$',str(where[k])): # refer another field
 						listw.append(f"{k}={where[k]}")
 					else:
-						listw.append(f"{k}=\"{1}\"".format(k,re.sub("\"","\\\"",str(where[k]))))
+						listw.append("{0}=\"{1}\"".format(k,re.sub("\"","\\\"",str(where[k]))))
 				where = " and ".join(listw)
 			else:
 				where = " and ".join(where)

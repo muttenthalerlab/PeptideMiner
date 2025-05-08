dbaf6c8a8fa47ff20bb8f9cd3d6681ea4c531e05
Johannes Zuegg <j.zuegg@uq.edu.au>
2025-05-08 10:37:29 +1000

    Step 4

--- PeptideMiner-master v1.0/utils/signalp_tasks.py
+++ PeptideMiner-master v1.0/utils/signalp_tasks.py
@@ -13,9 +13,10 @@ def run_signalp(seq_id,sequence,cutoff,signal_path,temp_path='/tmp'):
     with open(seq_file,'w') as tmp:
         tmp.write(f">{seq_id}\n{sequence}")
     
+    out_file = os.path.join(temp_path,f"{seq_id}.out")
     logger.info(f" [SingalP] -> ")
     # SignalP 4.1  : eukariotic 
-    cmd = f"{signal_path} -t euk -U {cutoff} -u {cutoff} {seq_file}"
+    cmd = f"{signal_path} -t euk -U {cutoff} -u {cutoff} {seq_file} > {out_file}"
     p = subprocess.run(cmd,shell=True,capture_output=True, text=True)
     ret = p.stdout
 

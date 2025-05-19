91d8465dcac7c6dc0c7b24c0ce4c850a6af8d493
JZuegg <j.zuegg@uq.edu.au>
2025-05-19 20:50:53 +1000

    Paper

--- lib/pipeline_tasks.py
+++ lib/pipeline_tasks.py
@@ -171,16 +171,16 @@ def read_cds(PM, Overwrite=False):
             # if no cds use original precursor
            _cds_lst.append({'seq_id':seq_id['id'],'n_cds':0,'cds':seq_id['precursor']})
     
-    # Upload and make unique by cds
-    _cds_id = {}
+    # Upload and make unique by cds_id
+    _cds_ids = []
     PM.cds_lst = []        
     for cds in _cds_lst:
         cds_id = upload_cds(PM.db,cds['cds'],cds['seq_id'])
-        if cds_id not in _cds_id:
+        if cds_id not in _cds_ids:
             cds['cds_id'] = cds_id
             PM.cds_lst.append(cds)
         else:
-            _cds_id[cds_id] = 1
+            _cds_ids.append(cds_id)
                    
     logger.info(f" [HMM Search] CDS: {len(PM.cds_lst)} uploaded")
 
